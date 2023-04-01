e37ae95ad8a6f69e424a989169c5a277d479e9bb
Noah Gift <noah.gift@gmail.com>
2023-04-01 14:39:21 +0000

    adding distroless working

--- rtorchdist/src/main.rs
+++ rtorchdist/src/main.rs
@@ -178,7 +178,7 @@ async fn main() -> std::io::Result<()> {
             .service(predict)
             .service(self_check)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
